d778866a00f9f296a8e3242ae9db57fb6e5ffb06
redsky0212 <wihyun0238@gmail.com>
2026-03-23 13:22:04 +0900

    설정수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'http://redsky0212.dothome.co.kr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/2026/react/mf-guide/',
+  baseUrl: '/2026/mf-next/guide/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
